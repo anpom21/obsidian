26f209898f3a6ae21194a8c24d4c281695632ecb
Andreas Pommerencke <anpom21@student.sdu.dk>
2026-06-11 22:40:32 +0200

    vault backup: 2026-06-11 22:40:32

--- Ubuntu/.obsidian/custom_scripts/send_notification.py
+++ Ubuntu/.obsidian/custom_scripts/send_notification.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 import argparse
+import requests
 
+ANDREAS_USER = "usozo22dr29waq7yx9f5u3ab74717x"
+ANDREAS_TOKEN = "aqy5q6psoain23br3bci8dxeund4ne"
 
-def send_notification(title: str, message: str) -> None:
-    # Replace this with your real notification logic.
-    print(f"{title}: {message}")
+def send_notification(title: str, message: str, priority: int = 0):
 
+    try:
+        requests.post(
+            "https://api.pushover.net/1/messages.json",
+            data={
+                "token": ANDREAS_TOKEN,
+                "user": ANDREAS_USER,
+                "title": title,
+                "message": message,
+                "priority": priority,
+            },
+            timeout=5,
+        )
+        print("Pushover notification sent successfully.")
+    except Exception as e:
+        print(f"Failed to send Pushover notification: {e}")
+        
 
 def main() -> None:
     parser = argparse.ArgumentParser()
